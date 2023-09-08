1b8550e11d265f5b4e24321fcfb7cb8aac11c09a
sergey <s.e.r.g.e.y88@mail.ru>
2023-09-08 21:05:55 +0300

    create new project Magento2

---
+++ app/etc/env.php
@@ -0,0 +1,82 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_1qwesu'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => '1tKlWvuHnNcKRWWQby64Fp8ln92Rt7hh'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '172_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '172_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'e9d0fa3241deec4a6cda3daef8676e1c'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'sergey',
+                'password' => 'Cthutq8!',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'install' => [
+        'date' => 'Fri, 08 Sep 2023 17:29:19 +0000'
+    ]
+];
---
+++ install.sh
@@ -0,0 +1,15 @@
+bin/magento setup:install \
+--base-url=http://magento2.loc \
+--db-host=localhost \
+--db-name=magento2 \
+--db-user=sergey \
+--db-password=Cthutq8! \
+--admin-firstname=admin \
+--admin-lastname=admin \
+--admin-email=admin@admin.com \
+--admin-user=admin \
+--admin-password=admin123 \
+--language=en_US \
+--currency=USD \
+--timezone=America/Chicago \
+--use-rewrites=1
